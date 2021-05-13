2be309955a51a599226a7fc33830a57695048360
subaru-saito1 <yuma.s8102@gmail.com>
2021-05-13 16:32:12 +0900

    各種イベントハンドラを追加

---
+++ js/classes.js
@@ -0,0 +1,13 @@
+
+
+/* 仮：キャンバス描画 */
+function drawCanvasKari() {
+  // 描画コンテキスト取得
+  const canvas = document.querySelector('canvas');
+  const ctx = canvas.getContext('2d');
+  // 背景描画
+  canvas.setAttribute('width', 400);
+  canvas.setAttribute('height', 400);
+  ctx.fillStyle = '#ffffff';
+  ctx.fillRect(0, 0, 400, 400);
+}
