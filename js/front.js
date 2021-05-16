c1d3c944380ea3712849e82204c239f8b3b1a9a3
subaru-saito1 <yuma.s8102@gmail.com>
2021-05-16 10:33:08 +0900

    マウス座標から盤面オブジェクトを特定する部分を実装

--- js/front.js
+++ js/front.js
@@ -164,18 +164,10 @@ function answerClear(evt) {
   console.log('解答消去');
 }
 
-
-/**
- * 盤面クリック
- */
-function clickBoard(evt) {
-  console.log('クリック');
-}
-
 /**
  * 盤面キーボード押下
  */
-function keyDownBoard(evt) {
+ function keyDownBoard(evt) {
   console.log('キーボード押下');
 }
 
@@ -184,4 +176,170 @@ function keyDownBoard(evt) {
  */
 function blurBoard(evt) {
   console.log('盤面フォーカス外れ');
+}
+
+/**
+ * 盤面クリック
+ */
+function clickBoard(evt) {
+  evt.preventDefault();   // 右クリックでメニューが開かないようにする
+  // objinfo概要
+  // type:cell => bi, bj, i, j の四つ組
+  // type:item => elidx, idx
+  // type:subel => elidx, subelidx 
+  // type:elem => elidx
+  let objinfo = identifyClickPos(evt.offsetX, evt.offsetY)
+  if (objinfo.type === 'cell') {
+    console.log(objinfo);
+  } else if (objinfo.type === 'item') {
+    console.log(objinfo);
+  } else if (objinfo.type === 'subel') {
+    console.log(objinfo);
+  } else if (objinfo.type === 'elem') {
+    console.log(objinfo);
+  } else {
+    console.log(objinfo, '(空白)');
+  }
+  Suiripuz.drawer.drawCanvas(Suiripuz.board);
+}
+
+/**
+ * マウス座標から盤面オブジェクトを特定する関数（大枠）
+ */
+function identifyClickPos(mx, my) {
+  let retobj = {}
+  mx -= Suiripuz.drawer.offset;
+  my -= Suiripuz.drawer.offset;
+  mainofsx = (Suiripuz.board.maxItemSize + 1) * Suiripuz.drawer.csize;
+  mainofsy = (Suiripuz.board.maxItemSize + 1) * Suiripuz.drawer.csize;
+  if (mx >= mainofsx && my >= mainofsy) {
+    // セル or 範囲外
+    retobj = identifyClickPosCell(mx - mainofsx, my - mainofsy);
+  } else if (mx >= mainofsx && my < mainofsy) {
+    // 上側要素枠 or 範囲外
+    retobj = identifyClickPosElYoko(mx - mainofsx, my);
+  } else if (mx < mainofsx && my >= mainofsy) {
+    // 左側要素枠 or 範囲外
+    retobj = identifyClickPosElTate(mx, my - mainofsy);
+  } else {
+    // 左上の枠外
+    retobj.type === 'none';
+  }
+  return retobj;
+}
+
+/**
+ * 補正済みマウス座標からセル位置を特定
+ */
+function identifyClickPosCell(mx, my) {
+  let obj = {}
+  // 変数名が長いのでエイリアス
+  let csize = Suiripuz.drawer.csize;
+  let elems = Suiripuz.board.numElems;
+  let items = Suiripuz.board.numItems;
+  // 座標値計算
+  let x = parseInt(mx / csize);
+  let y = parseInt(my / csize);
+  let bx = parseInt(x / items);
+  let by = parseInt(y / items);
+  let cx = parseInt(x % items);
+  let cy = parseInt(y % items);
+  // 範囲外バリデーション
+  if (bx + by >= elems - 1) {
+    obj = {'type': 'none'};
+  } else {
+    obj = {'type': 'cell', 'bi': bx, 'bj': by, 'i': cx, 'j': cy};
+  }
+  return obj;
+}
+
+/**
+ * 補正済みマウス座標から上要素オブジェクトを特定
+ */
+function identifyClickPosElYoko(mx, my) {
+  let obj = {}
+  // 例によってエイリアス
+  let csize = Suiripuz.drawer.csize;
+  let elems = Suiripuz.board.numElems;
+  let items = Suiripuz.board.numItems;
+  // 座標値計算
+  let x = Math.floor(mx / csize);
+  let y = Math.floor(my / csize);
+  // 範囲外バリデーション
+  if ((y < 0) || (x >= Suiripuz.board.maxCellSize)) {
+    obj = {'type': 'none'};
+  } else {
+    let bx = Math.floor(x / items);
+    let cx = parseInt(x % items);
+    let elidx = elems - bx - 1;
+    if (y === 0) {
+      // 要素
+      obj = {'type': 'elem', 'elidx': elidx};
+    } else if (y > 1) {
+      // 項目
+      obj = {'type': 'elem', 'elidx': elidx, 'idx': cx};
+    } else if (y === 1){
+      // サブ要素
+      let sbidx = identifySubelems(elidx, cx);
+      if (sbidx < 0) {
+        obj = {'type': 'elem', 'elidx': elidx, 'idx': cx};
+      } else {
+        obj = {'type': 'subel', 'elidx': elidx, 'subelidx': sbidx};
+      }
+    }
+  }
+  return obj;
+}
+
+/**
+ * 補正済みマウス座標から左要素オブジェクトを特定 
+ */
+function identifyClickPosElTate(mx, my) {
+  let obj = {}
+  // 例によってエイリアス
+  let csize = Suiripuz.drawer.csize;
+  let elems = Suiripuz.board.numElems;
+  let items = Suiripuz.board.numItems;
+  // 座標値計算
+  let x = Math.floor(mx / csize);
+  let y = Math.floor(my / csize);
+  // 範囲外バリデーション
+  if ((x < 0) || (y >= Suiripuz.board.maxCellSize)) {
+    obj = {'type': 'none'};
+  } else {
+    let by = Math.floor(y / items);
+    let cy = parseInt(y % items);
+    let elidx = by;
+    if (x === 0) {
+      // 要素
+      obj = {'type': 'elem', 'elidx': elidx};
+    } else if (x > 1) {
+      // 項目
+      obj = {'type': 'elem', 'elidx': elidx, 'idx': cy};
+    } else if (x === 1){
+      // サブ要素
+      let sbidx = identifySubelems(elidx, cy);
+      if (sbidx < 0) {
+        obj = {'type': 'elem', 'elidx': elidx, 'idx': cy};
+      } else {
+        obj = {'type': 'subel', 'elidx': elidx, 'subelidx': sbidx};
+      }
+    }
+  }
+  return obj;
+}
+
+/**
+ * 離散化座標からどのサブ要素に属するかを判定
+ * サブ要素に属さない場合は-1を返すものとする
+ */
+function identifySubelems(elidx, n) {
+  let i = 0; 
+  for (let subel of Suiripuz.board.elements[elidx].subelements) {
+    if (n >= subel.start && n < subel.start + subel.size) {
+      return i;
+    }
+    i++;
+  }
+  return -1;
 }
